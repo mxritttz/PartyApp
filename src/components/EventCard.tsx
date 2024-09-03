5989795daa9fe9f2c8f1dee40106596522596a2e
Jannes <janneshikmat@hotmail.de>
2024-09-03 12:12:23 +0200

    Add EventCard component

---
+++ src/components/EventCard.tsx
@@ -0,0 +1,22 @@
+import { EventModel } from "../models/Event";
+
+const img = "https://via.placeholder.com/300";
+
+type EventCardProps = {
+  event: EventModel;
+}
+
+const EventCard = ({ event }: EventCardProps) => {
+  return (
+    <div className="bg-gray-100 p-4 rounded-lg shadow-md mb-4">
+      <img src={img} alt="Event Thumbnail" className="rounded-md w-full"/>
+      <h2 className="mt-2 text-xl font-semibold">{ event.title }</h2>
+      <p className="text-gray-600">{ event.host }</p>
+      <p className="text-gray-600">{ event.date.toLocaleString() }</p>
+      <p className="text-gray-600">{ event.location }</p>
+      <p className="mt-2">{ event.description }</p>
+    </div>
+  )
+}
+
+export default EventCard;
