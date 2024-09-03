021d044f4666daea8302650ae5a2b66d858a4fae
Jannes <janneshikmat@hotmail.de>
2024-09-04 01:53:07 +0200

    Add navigation elements & auth links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,37 @@
+import { supabase } from '../supabase/client'
 import { Session } from "@supabase/supabase-js";
+import { useNavigate } from "react-router";
 
 type HeaderProps = {
     session?: Session|null|undefined;
 }
 
-const Header = ({}: HeaderProps) => {
+const Header = ({ session }: HeaderProps) => {
+  const navigate = useNavigate();
     return (
-        <header className="flex justify-between items-center p-4 bg-white shadow-md">
-        <h1 className="text-2xl font-bold">PartyPal</h1>
-        <div className="space-x-4">
+      <header className="flex justify-between items-center p-4 bg-white shadow-md">
+        
+        <div className="flex space-x-4">
+          <h1 className="text-2xl font-bold">PartyPal</h1>
+          
+          {session 
+            ? <button onClick={() => supabase.auth.signOut()}>Logout</button> 
+            : <button onClick={() => navigate("/auth")}>Login</button>
+          } 
+          
+          <button onClick={() => navigate("/")}>Home</button>
+          <button onClick={() => navigate("/event/add")}>Add Event</button>
+        </div>
+        
+        <div className="flex space-x-4">
           <button>
-            <img src="/icons/menu.svg" alt="Menu" />
+            <img src="/icons/menu.svg" alt="Menu"/>
           </button>
           <button>
-            <img src="/icons/search.svg" alt="Search" />
+            <img src="/icons/search.svg" alt="Search"/>
           </button>
         </div>
+      
       </header>
     )
 }
