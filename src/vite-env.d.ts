c559a9a05d7aa68861185815491b0f6b9354a935
Jannes <janneshikmat@hotmail.de>
2024-09-03 12:04:20 +0200

    Add env IntelliSense for TypeScript

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,17 @@
 /// <reference types="vite/client" />
+
 declare module "*.svg" {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-    export default content;
-  }
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_API_URL: string;
+}
+
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
