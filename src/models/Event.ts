c91caadd2f9291e98e3029c8cb98388453c85cbf
Jannes <janneshikmat@hotmail.de>
2024-09-04 01:51:12 +0200

    Add model EventData (same as Event but w/o id)

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,4 +1,14 @@
-export type EventModel = {
+export type EventData = {
+    title: string;
+    description: string;
+    host: string;
+    date: Date;
+    location: string;
+    metadata: any;
+}
+
+export type Event = {
+    id: string;
     title: string;
     description: string;
     host: string;
