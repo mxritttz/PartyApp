8303c1ad1550ccdbe188158b89df98f5b95d0994
Jannes <janneshikmat@hotmail.de>
2024-09-03 12:11:16 +0200

    Remove unnecessary import

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const BottomNavigation = () => {
   return (
     <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
